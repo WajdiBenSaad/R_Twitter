bd2500773fb56bf45b0f87dba0f127aac9562752
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2019-03-18 13:51:21 +0100

    Create Twitter Auth.r

---
+++ Twitter Auth.r
@@ -0,0 +1,23 @@
+# The packages we need are : 
+library(twitteR)
+library(httr)
+
+#Enter the info you had from Twitter when setting up your application:
+
+ api_key <- "your API key"
+ api_secret <- "your Secret key"
+ access_token <- "you Access Token"
+ access_token_secret <- "you Access Token Secret key"
+ 
+ #Now put these information into this function to setup your authentification:
+ setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+#If you get a message after this command, 
+# pick to store a local file of the authentification
+## [1] "Using direct authentication"
+
+#Now the fun part: lets collect some Tweets about Tunisia
+#we will get 1000 tweets with the word Tunisia in them:
+Tun_Tweets <- searchTwitter("Tunisia", n=1000)
+#lets see the Tweets:
+head(Tun_Tweets)
