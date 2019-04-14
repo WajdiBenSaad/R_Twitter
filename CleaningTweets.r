165b25b15270b3daaba5940c4d6c5f193fcd7dec
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2019-04-14 18:32:56 +0200

    Update CleaningTweets.r

--- CleaningTweets.r
+++ CleaningTweets.r
@@ -4,15 +4,21 @@ head(Tun_Tweets)
 tweets_df = twListToDF(Tun_Tweets)
 str(tweets_df)
 library(tm)
+
 mycorpus = Corpus(VectorSource(tweets_df))
 str(mycorpus)
+
 #Removing extra white-spaces
 mycorpus1 = tm_map(mycorpus, stripWhitespace)
+
 #converting text to lower case
 mycorpus2 = tm_map(mycorpus1, tolower)
+
 #removing  stopwords
 mycorpus3 = tm_map(mycorpus2, removeWords, stopwords("english"))
+
 #removing punctuation symbols
 mycorpus4 = tm_map(mycorpus3, removePunctuation)
+
 #removing numbers
 mycorpus5 = tm_map(mycorpus4, removeNumbers)
