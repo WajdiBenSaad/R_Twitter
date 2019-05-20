5c1c3a14c4e8eb33b025e56ace5319669ffdef57
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2019-05-20 13:47:50 +0200

    Update Twitter_Auth.r

--- Twitter_Auth.r
+++ Twitter_Auth.r
@@ -3,7 +3,7 @@
 library(twitteR) 
 library(httr) 
 
-#Enter the info you had from Twitter when setting up your application:
+## Enter the info you had from Twitter when setting up your application:
 ## These information can be retreivedfrom here : https://developer.twitter.com/en/docs/basics/authentication/guides/access-tokens.html
 ## You need to set up a Twitter account if you don't have one, and create an application with that user account.
 
@@ -12,5 +12,5 @@ library(httr)
  access_token <- "you Access Token??"
  access_token_secret <- "you Access Token Secret key??"
  
- #Now put these information into this function to setup your authentification:
+ # Now put these information into this function to setup your authentification:
  setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
